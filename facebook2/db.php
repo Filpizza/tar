236dceab76bcb368f6900f66bf5fdff027b37be4
Taras <tar_rudenko@ukr.net>
2018-03-12 17:32:33 +0200

    1commit

--- facebook2/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$link = mysqli_connect('localhost', 'root', '', 'test');
-
-	if (mysqli_connect_errno()) {
-  		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  	}
-
-  	session_start();
